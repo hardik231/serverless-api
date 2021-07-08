e14e7ec4e1aab3d1e19516fc6027c2d8339449fa
hardik231 <hrkumar231@gmail.com>
2021-07-07 23:16:12 -0700

    first

---
+++ handler.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const AWS = require('aws-sdk'); 
+
+
+//AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.submit = async (event) => {
+  const requestBody = JSON.parse(event.body);
+  const fullname = requestBody.fullname;
+  const email = requestBody.email;
+  const experience = requestBody.experience;
+
+  if (typeof fullname !== 'string' || typeof email !== 'string' || typeof experience !== 'number') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t submit candidate because of validation errors.'));
+    return;
+  }
+
+  submitCandidateP(candidateInfo(fullname, email, experience))
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully submitted candidate with email ${email}`,
+          candidateId: res.id
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to submit candidate with email ${email}`
+        })
+      })
+    });
+};
+
+const submitCandidateP = candidate => {
+  console.log('Submitting candidate');
+  const candidateInfo = {
+    TableName: 'candidates',
+    Item: candidate,
+  };
+  return dynamoDb.put(candidateInfo).promise()
+    .then(res => candidate);
+};
+
+const candidateInfo = (fullname, email, experience) => {
+  const timestamp = new Date().getTime();
+  return {    
+    fullname: fullname,
+    email: email,
+    experience: experience,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+  };
+};
